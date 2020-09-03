fcb22c005f2d66e59cce3c3d8b4c63e04b7beb40
Nicolas <nicolas.7l@hotmail.com>
2020-09-03 11:48:24 -0300

    Finalizacao

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,63 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
+import path from 'path';
+import uploadConfig from '../config/upload';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionsService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransactionsService = new CreateTransactionsService();
+
+  const transaction = await createTransactionsService.execute({
+    title,
+    category,
+    type,
+    value,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransactionService = new DeleteTransactionService();
+
+  await deleteTransactionService.execute(id);
+
+  return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactionsServices = new ImportTransactionsService();
+
+    const filePath = path.join(uploadConfig.directory, request.file.filename);
+
+    const transactions = await importTransactionsServices.execute(filePath);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError('insuficient balance for this transaction');
+    }
+
+    let categoryExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExists) {
+      categoryExists = categoryRepository.create({
+        title: category,
+      });
+      await categoryRepository.save(categoryExists);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category: categoryExists,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
